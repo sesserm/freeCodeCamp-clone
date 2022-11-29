d75b181743f8cfafb704e9fa08405ab52d01a839
msesser <sessermatias@gmail.com>
2022-11-28 21:44:54 -0300

    Primer proyecto con React

---
+++ src/componentes/testimonio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../hojas-de-estilo/testimonio.css'
+
+function Testimonio(props) {
+    return (
+        <div className = 'contenedor-testimonio'>
+            <img className='imagen-testimonio' src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+            alt='imagen-emma'/>
+            <div className ='contenedor-texto-testimonio'>
+                <p className='nombre-testimonio'><strong>{props.nombre}</strong> in {props.pais}</p>
+                <p className = 'cargo-testimonio'>{props.cargo} at <strong>{props.empresa}</strong></p>
+                <p className='texto-testimonio'>"{props.testimonio}"</p>
+            </div>
+        </div>
+    );
+}
+
+export default Testimonio
